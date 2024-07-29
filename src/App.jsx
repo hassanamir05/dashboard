a51e2ff4a2aded2adf958ef173c8e401ee726fd4
Muhammad Hassan Amir <hassanamir05@gmail.com>
2024-07-29 03:15:50 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <h1 className="text-3xl font-lighter text-red-500 hover:text-yellow-400 ease-in-out hover:cursor-pointer">
+      Hello world!
+    </h1>
+  );
+}
