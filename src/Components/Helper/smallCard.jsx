330fb681a83e011c4e4330891734e433b4209b74
M.Hassan Amir <hassanamir0506@gmail.com>
2024-08-14 19:47:39 +0500

    completed loan section

--- src/Components/Helper/smallCard.jsx
+++ src/Components/Helper/smallCard.jsx
@@ -2,10 +2,7 @@ const SmallCard = (props) => {
     console.log('inside small cards component');
 
     return (
-        <div
-            style={{ fontFamily: 'Inter' }}
-            className="rounded-[20px] bg-white flex flex-row justify-center items-center h-[90px] p-4 w-auto min-w-[167px] max-w-[300px]"
-        >
+        <div className="card bg-white h-auto py-5 px-3 flex flex-row rounded-[15px] m-3 flex-grow justify-center max-h-[90px]">
             <div
                 className="w-[50px] h-[50px] rounded-[50%] flex justify-center items-center"
                 style={{ backgroundColor: props.bgColor }}
@@ -13,7 +10,7 @@ const SmallCard = (props) => {
                 <img src={props.iconSrc} alt="icon" className="w-[20px] h-[20px]" />
             </div>
 
-            <div className="flex flex-col items-center justify-between py-1 ml-2">
+            <div className="flex flex-col items-center justify-between py-1 ml-3 h-[40px]">
                 <span className="text-[12px]">{props.title}</span>
                 <span className="font-semibold text-[16px]">{props.value}</span>
             </div>
--- src/Sections/Loan.jsx
+++ src/Sections/Loan.jsx
@@ -1,5 +1,54 @@
+import SmallCard from "../Components/Helper/smallCard";
+import LoanTableComponent from "../Components/Helper/LoanTable";
+import ComponentHeading from '../Components/Helper/ComponentHeading'
+
+const smallCardsData = [
+  {
+    title: 'Personal Loan',
+    amount: '$50,000',
+    iconSrc: '/src/assets/loanSectionIcons/personal-loan.png',
+    bgColor: '#E7EDFF',
+  },
+  {
+    title: 'Cooperate Loan',
+    amount: '$100,000',
+    iconSrc: '/src/assets/loanSectionIcons/cooperate-loan.png',
+    bgColor: '#FFF5D9',
+  },
+  {
+    title: 'Business Loan',
+    amount: '$500,000',
+    iconSrc: '/src/assets/loanSectionIcons/business-loan.png',
+    bgColor: '#FFE0EB',
+  },
+  {
+    title: 'Custom Loan',
+    amount: '$7,920',
+    iconSrc: '/src/assets/loanSectionIcons/custom-loan.png',
+    bgColor: '#DCFAF8',
+  },
+]
+
+
+
 const Loan = () => {
-  return <h1>Loan</h1>;
+  return <div className="bg-[#E5E5E5] min-h-screen overflow-y-auto overflow-x-hidden py-10 min-w-screen flex flex-wrap justify-center gap-4 px-5">
+
+
+    <div className="flex gap-2 flex-row justify-evenly w-full flex-wrap">
+      {
+        smallCardsData.map((cardData, index) => {
+          return <SmallCard title={cardData.title} iconSrc={cardData.iconSrc} value={cardData.amount} bgColor={cardData.bgColor} />
+        })
+      }
+    </div>
+
+    <div className="flex flex-col">
+      <ComponentHeading name="Active Loans Overview" />
+      <LoanTableComponent />
+    </div>
+
+  </div>
 };
 
 export default Loan;
