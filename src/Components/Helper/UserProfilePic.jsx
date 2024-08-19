3c37d38adba9312967993d274e605e3f2d258165
M.Hassan Amir <hassanamir0506@gmail.com>
2024-08-19 11:28:38 +0500

    fixed all errors

--- src/Components/Helper/UserProfilePic.jsx
+++ src/Components/Helper/UserProfilePic.jsx
@@ -3,4 +3,4 @@ const ProfilePic = (props) => {
     return <img src={props.src} className="h-[35px] ml-2 hover:cursor-pointer " alt="Profile Picture" />
 }
 
-export default ProfilePic;
+export default ProfilePic; 
